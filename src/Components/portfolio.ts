a9d9ac417c01b7e4e52746f7162610705f36d492
Alain Leriche <alaleric@publicisgroupe.net>
2024-09-09 13:36:05 -0400

    desktop version is in good shape, starting the mobile version

--- src/Components/portfolio.ts
+++ src/Components/portfolio.ts
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-//////////////////////////////////////////////// OPENING /////////////////////////////////////
+////////////////////////////////// INITIALIZATION / HISTORY ///////////////////////////////////
 
     const portfolioPage = $('#portfolioPage');
 
@@ -9,15 +9,12 @@ document.addEventListener("DOMContentLoaded", function() {
             // Asynchronously load portfolio page content
             await loadPageContent(portfolioPage, 'portfolio.html');
             $('#mainContainer').hide();
-
             // Ensure DOM is ready before fading in the content
             $(function () {
-                $('#mainContainer').fadeIn(600);
+                $('#mainContainer').fadeIn(800);
             });
-
             // Push the initial state to the history
             history.replaceState({ section: 'portfolio' }, 'portfolio', '#portfolio');
-
             // Set initial state of portfolio button
             $('.nav-btn[data-name="portfolio"]').addClass('active').prop('disabled', true);
         } 
@@ -41,14 +38,13 @@ document.addEventListener("DOMContentLoaded", function() {
     setInitialState();
 
     const initialSection = window.location.hash.replace('#', '') || 'portfolio';
-    
     // Find the corresponding navigation button and trigger its click handler programmatically
     const initialNavButton = document.querySelector(`.nav-btn[data-name="${initialSection}"]`);
     if (initialNavButton) {
         initialNavButton.dispatchEvent(new Event('touchend'));
     }
 
-    //////////////////////////////////////////////// MAIN NAVIGATION /////////////////////////////
+    ////////////////////////////////////////// MAIN NAVIGATION / HISTORY //////////////////////////
 
     // Define the page load status type
     type PageLoadStatus = {
@@ -64,16 +60,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Function to handle loading and history management
     async function loadPage(section: string, addToHistory = true) {
+
         const sectionPath: string = `${section}.html`,  // Path to the section's HTML file
             pageId: string = `#${section}Page`,  // Dynamically generate the ID selector for the page
             page = $(pageId);  // Get the page element by ID
-
             // For testing
-            // console.log(`Loading page sample from: ${sectionPath}`);
-
+            console.log(`Loading page sample from: ${sectionPath}`);
         // Hide all pages first
         $('.pages').fadeOut(400);
-
         // Check if the section content has not been loaded yet
         if (!pageLoadStatus[section]) {
             // Load the content for the selected section
@@ -91,10 +85,8 @@ document.addEventListener("DOMContentLoaded", function() {
             page.fadeIn(500);
             document.body.style.overflowY = 'auto'; // Re-enable scrolling after fade-in is complete
         });
-
         // Optionally lock body scrolling when switching sections
         document.body.style.overflowY = 'hidden';
-
         // Add the state to the browser history
         if (addToHistory) {
             history.pushState({ section }, section, `#${section}`);
@@ -103,41 +95,77 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Attach event listener to all navigation buttons
     $('.nav-btn').on('click touchend', function () {
-
         const thisNavButton = $(this),  // Reference the clicked button
             section: string = thisNavButton.data('name');  // Get the section name from data-name attribute
-
         // Update the active button states
         $('.nav-btn').removeClass('active').prop('disabled', false);
         thisNavButton.addClass('active').prop('disabled', true);
-
         // Load the page and add it to the history
         loadPage(section);
-        
     });
+
+    //////////////////////////////////// HISTORY MANAGEMENT ///////////////////////////////////
 
     // Event listener for browser back/forward buttons
     window.onpopstate = async function (event) {
-        if (event.state && event.state.section) {
-            const section = event.state.section;
-
-            // Update the navigation button states
-            $('.nav-btn').removeClass('active').prop('disabled', false);
-            $(`.nav-btn[data-name="${section}"]`).addClass('active').prop('disabled', true);
-
-            // Load the page corresponding to the history state
-            await loadPage(section, false); 
+        if (event.state) {
+            if (event.state.section) {
+                const section = event.state.section;
+                // Update the navigation button states
+                $('.nav-btn').removeClass('active').prop('disabled', false);
+                $(`.nav-btn[data-name="${section}"]`).addClass('active').prop('disabled', true);
+                await loadPage(section, false); // Load page without adding to history
+                // If a sample is open, close it when navigating to a different section
+                if (samples.is(':visible')) {
+                    triggerEvent(); // Close the sample page
+                }
+            } else if (event.state.sample) {
+                const sampleName = event.state.sample,
+                    samplePath = `samples/${sampleName}.html`;
+                try {
+                    // Load the page corresponding to the history state
+                    await loadSample(samplePath, false); // Load sample without adding to history
+                    samples.fadeIn(600);
+                    document.body.style.overflowY = 'hidden';
+                } catch (error) {
+                    console.error(`Error loading sample: ${error}`);
+                }
+            }
         }
     };
 
-    //////////////////////////////////////////////// LOADING SAMPLES /////////////////////////////////////
+    ///////////////////////////////// LOADING SAMPLES / HISTORY //////////////////////////////////
 
-    let samples: JQuery<HTMLElement> = $('#samples');
+    let samples: JQuery<HTMLElement> = $('#samples'),
+        overlayContainer: JQuery<HTMLElement> = $('.overlayContainer');
 
-    async function loadSample(samplePath: string) {
+    async function loadSample(samplePath: string, addToHistory = true) {
         return new Promise<void>((resolve, reject) => {
             samples.load(samplePath, function (responseTxt, statusTxt, xhr) {
                 if (statusTxt === "success") {
+                    // let overlayContainer: JQuery(HTMLElement) = $('.overlayContainer');
+                    // alert(overlayContainer);
+                    // const sampleElement = overlayContainer.get(0);
+                    // if (sampleElement) {
+                    //     sampleElement.scrollTo(0, 0); // Reset scroll position to top
+                    //     alert(sampleElement);
+                    // }
+                    // overlayContainer.scrollTop(0);
+                    // console.log("Sample loaded successfully.");
+                    // samples.hide(); // Hide the sample container first
+                    // samples.scrollTop(0); // Reset scroll position to top
+                    // overlayContainer.scrollTop(0);
+                    // console.log("Scroll position reset.");
+
+
+                    if (addToHistory) {
+                        // Extract the sample name from the path (assuming it follows 'samples/sampleName.html')
+                        const sampleName = samplePath.split('/').pop()?.replace('.html', '');
+                        // Push the state to the history
+                        history.pushState({ sample: sampleName }, sampleName || '', `#sample-${sampleName}`);
+                    }
+                    // Reset scroll position to top of the sample container
+                    // samples.scrollTop(0);
                     resolve(); // Make sure to resolve the promise
                     // samples.fadeIn(600);
                     // document.body.style.overflowY = 'hidden';
@@ -148,34 +176,31 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    // Closing the sample page
     async function triggerEvent() {
-        samples.fadeOut(400);
-        // setTimeout(() => {
-            document.body.style.overflowY = 'auto';
-        // }, 400);
+        samples.fadeOut(400, function () {
+            document.body.style.overflowY = 'auto'; // Re-enable the main page's scroll
+        });
     }
 
     // from parents
     $(document).on('click touchend', '.buttonSample', async function () {
-
-        let samplePath: string = 'samples/' + $(this).data('name') + ".html";
-
+        const samplePath: string = 'samples/' + $(this).data('name') + ".html";
         // For testing
-        // console.log(`Loading page sample from: ${samplePath}`);
-    
+        console.log(`Loading page sample from: ${samplePath}`);
         try {
             await loadSample(samplePath);
             samples.fadeIn(600);
             // Hide the main scroller
             document.body.style.overflowY = 'hidden';
-    
             // Set up the close button event
-            // $('.overlayContainer').on('click touchend', '.closeBtn', function () {
             $('.overlayContainer').on('click touchend', function (event) {
                 // Determine if the click was on .closeBtn
                 if ($(event.target).hasClass('closeBtn')) {
                     triggerEvent();
-                } else triggerEvent();
+                } else {
+                    triggerEvent();
+                }
             });
         } 
         catch (error) {
